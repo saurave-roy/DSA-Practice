05ef28afa8440a7d7a4282f5ac5b29b02f2ddc67
saurave-roy <saurave.roy04@gmail.com>
2026-08-29 11:29:14 +0530

    Update Middle of the Linked List.java

--- Linked List/Middle of the Linked List.java
+++ Linked List/Middle of the Linked List.java
@@ -1,3 +1,29 @@
+Bruteforce:
+class Solution {
+    public ListNode middleNode(ListNode head){
+        ListNode temp = head;
+        int cnt = 0;
+
+        while(temp!=null){
+            cnt++;
+            temp = temp.next;
+        }
+        int midNode = (cnt/2)+1;
+        temp = head;
+
+        while(temp != null){
+            midNode = midNode - 1;
+
+            if(midNode == 0)break;
+
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
+
+
+Optimal:
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
