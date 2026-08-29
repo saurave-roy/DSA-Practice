a4bd5ab628c091637ea70d6584d90033d3774236
saurave-roy <saurave.roy04@gmail.com>
2026-08-29 11:46:56 +0530

    Update Reverse Linked List.java

--- Linked List/Reverse Linked List.java
+++ Linked List/Reverse Linked List.java
@@ -17,3 +17,19 @@ class Solution {
         return head;
     }
 }
+
+Optimal:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = head;
+        
+        while(temp!=null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}
