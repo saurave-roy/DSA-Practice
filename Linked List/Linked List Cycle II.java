2931521d4b35268c5819a498524dc6df3edfff78
saurave-roy <saurave.roy04@gmail.com>
2026-08-30 13:16:42 +0530

    Create Linked List Cycle II.java

---
+++ Linked List/Linked List Cycle II.java
@@ -0,0 +1,41 @@
+Brute force:
+
+  public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Map<ListNode, Integer> map = new HashMap<>();
+        ListNode temp = head;
+
+        while(temp!=null){
+            if(map.containsKey(temp))return temp; 
+            map.put(temp,1);
+            temp = temp.next;
+        }
+        return null;
+    }
+}
+
+Optimal:
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                while(slow == fast){
+                    slow = head;
+                    while(slow != fast){
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                    return slow;
+                }
+            }
+        }
+        return null;
+    }
+}
